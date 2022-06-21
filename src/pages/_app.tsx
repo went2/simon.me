174a657d616583aec91541c8e2b0ef8091ff34be
wentao <569845499@qq.com>
2022-06-21 14:59:40 +0800

    polish index

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,33 @@
 import '../styles/global.scss';
 import type { AppProps } from 'next/app';
+import {useState, useEffect} from 'react';
+import Head from 'next/head';
+import Layout from '../components/Layout';
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [title, setTitle] = useState('Open');
+
+  useEffect(() => {
+    // browser api need to be execucted in useEffect
+    document.addEventListener('visibilitychange', () => {
+      if (document.visibilityState === 'hidden') {
+        setTitle('Closed');
+      } else {
+        setTitle('Open');
+      }
+    });
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>WestDoor is {title}</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 
 export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,19 @@
 import { NextPage } from 'next';
-import Layout from '../components/Layout';
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-
-import { useState, useEffect } from 'react';
+import styles from '../styles/Home.module.scss';
 
 const Home: NextPage = () => {
-  const [title, setTitle] = useState('Open');
-
-  useEffect(() => {
-    // browser api need to be execucted in useEffect
-    document.addEventListener('visibilitychange', () => {
-      if (document.visibilityState === 'hidden') {
-        setTitle('Closed');
-      } else {
-        setTitle('Open');
-      }
-    });
-  }, [])
-
   return (
-    <>
-      <Head>
-        <title>WestDoor is {title}</title>
-      </Head>
-      <Layout>
-        <main>
-          <div>THE WESTDOOR</div>
-          <div>耶稣在加利利海边行走的时候，看见兄弟二人，就是名叫彼得的西门和他的弟弟安得烈，正在把网撒到海里去；他们是渔夫。 19 耶稣就对他们说：“来跟从我，我要使你们作得人的渔夫。” 20 他们立刻撇下网，跟从了他。</div>
-          <div>————【马太福音：4:18-20】</div>
-        </main>
-      </Layout>
-    </>
+    <main className={styles.home}>
+      <div className={styles.title}>
+        <p>WEST DOOR</p>
+      </div>
+      <div className={styles.quote}>
+        <div>耶稣在加利利海边行走的时候，看见兄弟二人，就是名叫彼得的<strong>西门</strong>和他的弟弟安得烈，正在把网撒到海里去；他们是渔夫。耶稣就对他们说：“来跟从我，我要使你们作得人的渔夫。”他们立刻撇下网，跟从了他。</div>
+        <div className={styles.source}>
+          <p>马太福音：4:18-20</p>
+        </div>
+      </div>
+    </main>
   );
 };
 
