21ecd5433d7b2c82c2d7de8e58b99e02d1dfb774
ywt <569845499@qq.com>
2022-06-29 00:39:29 +0800

    add personal summary

--- src/pages/cv.tsx
+++ src/pages/cv.tsx
@@ -22,7 +22,7 @@ const Cv: NextPage = () => {
       </div>
 
       <CollapseContent title='个人总结'>
-        M.S. degree in Biology, worked as a research assistant and a science editor, before starting to build career in software developing. Spent one year in self-learning, then got accepted by San Francisco State University and granted M.S. degree in Computer Science. Now working on Android development as a software engineer.
+        本科与硕士就读教育技术学专业，毕业后在教育服务公司先后从事教学设计、产品运营等工作，后花半年时间学习前端开发相关知识与技能，目前从事前端开发，有后台管理系统、H5小游戏、移动端H5等类型应用的开发经验。
       </CollapseContent>
 
       <div className={styles.panel}></div>
