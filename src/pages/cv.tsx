13c66f55b84ec8d94ce99c961dec75464257bc71
simon <simonfisher227@gmail.com>
2022-11-02 23:26:54 +0800

    feat: add 3 svg icons

--- src/pages/cv.tsx
+++ src/pages/cv.tsx
@@ -12,21 +12,37 @@ const Cv: NextPage = () => {
       <div className={styles.info}>
         <div className={styles.photo}></div>
         <div className={styles.baseInfo}>
-          <h1>俞文韬</h1>
-          <p>Front-End Engineer</p>
-          <div className={styles.contact}>
-            <p>simonfisher227@gmail.com | 上海</p>
+          <div className={styles.title}>
+            <strong className={styles.name}>俞文韬</strong>
+            <div className={styles.job}>Front-End Engineer</div>
           </div>
-          <div className={styles.socialMedia}>
-            <a href="https://github.com/went2">
-              Github
-            </a>
+          <div className={styles.items}>
+            <div className={styles.item}>
+              <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3795" width="18" height="18">
+                <path d="M853.333333 170.666667 170.666667 170.666667C123.733333 170.666667 85.333333 209.066667 85.333333 256l0 512c0 46.933333 38.4 85.333333 85.333333 85.333333l682.666667 0c46.933333 0 85.333333-38.4 85.333333-85.333333L938.666667 256C938.666667 209.066667 900.266667 170.666667 853.333333 170.666667zM853.333333 768l-85.333333 0L768 392.533333 512 554.666667 256 392.533333 256 768 170.666667 768 170.666667 256l51.2 0 290.133333 179.2L802.133333 256 853.333333 256 853.333333 768z" p-id="3796" fill="#2c2c2c"></path>
+              </svg>
+              simonfisher227@gmail.com
+            </div>
+            <div className={styles.item}>
+              <svg viewBox="0 0 1024 1024" version="1.1" fill="#2c2c2c" xmlns="http://www.w3.org/2000/svg" p-id="4051" xmlnsXlink="http://www.w3.org/1999/xlink" width="18" height="18">
+                <path d="M661.333333 42.666667l-341.333333 0C261.12 42.666667 213.333333 90.453333 213.333333 149.333333l0 725.333333C213.333333 933.546667 261.12 981.333333 320 981.333333l341.333333 0c58.88 0 106.666667-47.786667 106.666667-106.666667l0-725.333333C768 90.453333 720.213333 42.666667 661.333333 42.666667zM490.666667 938.666667c-35.413333 0-64-28.586667-64-64s28.586667-64 64-64 64 28.586667 64 64S526.08 938.666667 490.666667 938.666667zM682.666667 768 298.666667 768 298.666667 170.666667l384 0L682.666667 768z" p-id="4052"></path>
+              </svg>
+              18521710097
+            </div>
+            <div className={styles.item}>
+              <svg fill="#2c2c2c" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4925" xmlnsXlink="http://www.w3.org/1999/xlink" width="18" height="18">
+                <path d="M941.714 512q0 143.433-83.712 258.011t-216.283 158.574q-15.433 2.853-22.565-3.986t-7.131-17.152v-120.576q0-55.442-29.696-81.152 32.585-3.438 58.587-10.277t53.723-22.272 46.299-37.998 30.281-60.014 11.703-86.016q0-69.157-45.129-117.723 21.138-52.005-4.571-116.553-16.018-5.157-46.299 6.29t-52.553 25.161l-21.723 13.714q-53.138-14.848-109.714-14.848t-109.714 14.848q-9.143-6.29-24.283-15.433t-47.726-22.016-49.152-7.717q-25.161 64.585-3.986 116.553-45.129 48.567-45.129 117.723 0 48.567 11.703 85.723t29.989 60.014 46.007 38.29 53.723 22.272 58.587 10.277q-22.857 20.553-28.014 58.843-11.995 5.705-25.71 8.558t-32.585 2.853-37.413-12.288-31.707-35.73q-10.862-18.286-27.721-29.696t-28.27-13.714l-11.447-1.719q-11.995 0-16.567 2.56t-2.853 6.583 5.157 8.009 7.424 6.839l3.986 2.853q12.581 5.705 24.869 21.723t17.993 29.147l5.705 13.129q7.424 21.723 25.161 35.145t38.29 17.152 39.717 3.986 31.707-2.011l13.129-2.304q0 21.723 0.293 50.871t0.293 30.866q0 10.277-7.424 17.152t-22.857 3.986q-132.571-43.995-216.283-158.574t-83.712-258.011q0-119.442 58.843-220.27t159.707-159.707 220.27-58.843 220.27 58.843 159.707 159.707 58.843 220.27z" p-id="4926"></path>
+              </svg>
+              <a href="https://github.com/went2">
+                https://github.com/went2
+              </a>
+            </div>
           </div>
         </div>
       </div>
 
       <CollapseContent title='个人简介'>
-        教育技术学硕士毕业，在教育公司从事教学设计、产品运营等工作。之后自学前端开发相关知识，目前从事前端开发，参与过后台管理系统、H5小游戏、移动端H5等类型应用的开发。
+        教育技术学硕士毕业，在教育公司从事教学设计、产品运营等工作。后自学前端开发技术，目前从事前端开发，参与过后台管理系统、H5小游戏、移动端H5等类型应用的开发。
       </CollapseContent>
 
       <CollapseContent title='工作经历'>
@@ -50,7 +66,7 @@ const Cv: NextPage = () => {
             <section className={styles.jobDes}>
               <p><strong>2.	H5 小游戏</strong></p>
               <p>概述：在移动端玩的 H5 小游戏，规模如7k7k的“翻牌小游戏”。使用 Egret 引擎开发，build成压缩包，在 App 端下载使用。</p>
-              <p>我的角色：调研与独立开发H5小游戏：前期花了4天调研 Egret 系列工具（引擎API、龙骨动画制作工具），开发周期内与产品经理沟通需求，设计小游戏状态、资源、视图的管理，完成3个小游戏开发与部署。期间对 Egret 引擎Api、DragonBone 龙骨动画有较多使用。</p>
+              <p>我的角色：调研与独立开发H5小游戏：前期花4天调研 Egret 系列工具（引擎API、龙骨动画制作工具），开发周期内与产品经理沟通需求，设计小游戏状态、资源、视图的管理，完成3个小游戏开发与部署。期间对 Egret 引擎Api、DragonBone 龙骨动画有较多使用。</p>
               <p>项目周期：2021.11-2021.12</p>
             </section>
 
@@ -151,9 +167,9 @@ const Cv: NextPage = () => {
               <CompanyName>江南大学 | 2014年9月-2017年6月 | 无锡</CompanyName>
             </div>
             <Image
-              src="/jiangnan_university_logo.png" alt="jiangnan_university_logo"
-              width={80}
-              height={80}
+              src="/images/jiangnan_university_logo.png" alt="jiangnan_university_logo"
+              width={75}
+              height={75}
             />
           </header>
           <div className={styles.expDesEdu}>
@@ -193,9 +209,9 @@ const Cv: NextPage = () => {
               <CompanyName>江南大学 | 2010年9月-2014年6月 | 无锡</CompanyName>
             </div>
             <Image
-              src="/jiangnan_university_logo.png" alt="jiangnan_university_logo"
-              width={80}
-              height={80}
+              src="/images/jiangnan_university_logo.png" alt="jiangnan_university_logo"
+              width={75}
+              height={75}
             />
           </header>
 
