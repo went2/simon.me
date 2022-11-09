0f008f27251f1a907f2579439bae50a83a2a1d4d
simon <simonfisher227@gmail.com>
2022-11-09 23:36:34 +0800

    feat: change navbar style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,29 +5,24 @@ import Link from 'next/link';
 const Header: NextPage = () => {
 
   return (
-    <div className={styles.header}>
-      <Link href='/'>
-        首页
-      </Link>
-
-      <ul className={styles.headerNav}>
-        <li>
-          <Link href='/articles'>
-            文章
-          </Link>
-        </li>
-        <li>
-          <Link href='/projects'>
-            项目
-          </Link>
-        </li>
-        <li>
-          <Link href='/cv'>
-            CV
-          </Link>
-        </li>
-      </ul>
-    </div>
+    <header className={styles.header}>
+      <section className={styles.headerNav}>
+        <ul>
+          <li>
+            <Link href='/'>首页</Link>
+          </li>
+          <li>
+            <Link href='/articles'>文章</Link>
+          </li>
+          <li>
+            <Link href='/projects'>项目</Link>
+          </li>
+          <li>
+            <Link href='/cv'>CV</Link>
+          </li>
+        </ul>
+      </section>
+    </header>
   );
 };
 
