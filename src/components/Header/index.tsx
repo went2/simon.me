7261c95a2029789111974c2cbcc38608c5ab997b
simon <simonfisher227@gmail.com>
2022-11-12 00:20:24 +0800

    refac: re-style header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,30 +4,36 @@ import Link from 'next/link';
 import { INavItem } from '../../utils/navbar';
 
 const navList: INavItem[] = [
-  { name: '首页', path: '/', icon: 'fa fa-home' },
-  { name: '文章', path: '/articles', icon: 'fa fa-book' },
-  { name: '项目', path: '/projects', icon: 'fa fa-product-hunt' },
-  { name: 'cv', path: '/cv', icon: 'fa fa-at' },
+  { name: '/ 项目', path: '/projects', icon: 'fa fa-product-hunt' },
+  { name: '/ 文章', path: '/articles', icon: 'fa fa-book' },
+  { name: '/ 关于我', path: '/about', icon: 'fa fa-at' },
 ];
 
-
 const Header: NextPage = () => {
-  
   return (
     <header className={styles.header}>
       <section className={styles.headerNav}>
-        <ul>
-          {
-            navList.map((item, index) => (
-              <li key={index}>
-                <Link href={item.path}>
-                  <i className={item.icon}></i>
-                  {'\u00A0'} {item.name}
-                </Link>
-              </li>
-            ))
-          }
-        </ul>
+        <div className={styles.navTop}>
+          <h1>
+            <Link href='/'>Simon Fisher</Link>
+          </h1>
+          <div className={styles.navLinks}>
+            <ul>
+              {
+                navList.map((item, index) => (
+                  <li key={index}>
+                    <Link href={item.path}>
+                       {item.name}
+                    </Link>
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
+        </div>
+        <p className={styles.indexAbout}>
+          我是一名前端工程师，大学就读教育技术学专业，毕业后在K12教育培训公司从事过教学设计、产品运营等工作。后自学前端开发，开发过后台管理系统、H5小游戏、移动端页面、小程序等应用。
+        </p>
       </section>
     </header>
   );
