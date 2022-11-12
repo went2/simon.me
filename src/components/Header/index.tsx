9d87fabebe073d0be291768a11582d7b745f32e1
simon <simonfisher227@gmail.com>
2022-11-12 15:44:03 +0800

    add 3d rotate effect on header!

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import { NextPage } from "next";
 import styles from './Header.module.scss';
 import Link from 'next/link';
 import { INavItem } from '../../utils/navbar';
+import throttle from "../../utils/throttle";
 
 const navList: INavItem[] = [
   { name: '/ 项目', path: '/projects', icon: 'fa fa-product-hunt' },
@@ -10,13 +11,41 @@ const navList: INavItem[] = [
 ];
 
 const Header: NextPage = () => {
+  const rotate3dEffect = throttle((event: React.MouseEvent<HTMLHeadingElement>) => {
+    const MOSTX = 12;
+    const MOSTY = 12;
+
+    const ele = event.target as HTMLHeadingElement;
+    ele.style.transition = 'all .2s ease-in-out';
+    
+    const { offsetX, offsetY } = event.nativeEvent;
+    const { width, height } = ele.getBoundingClientRect();
+    const halfWidth = width/2;
+    const halfHeight = height/2;
+
+    // calculate angle
+    const rotateX = ((offsetX - halfWidth ) / halfWidth) * MOSTX;
+    const rotateY = ((offsetY - halfHeight) / halfHeight) * MOSTY;
+    
+    // set the rotation
+    ele.style.transform = `rotateY(${rotateY}deg) rotateX(${rotateX}deg)`;
+  }, 200);
+
+  const setBack = (event: React.MouseEvent<HTMLHeadingElement>) => {
+    const ele = event.target as HTMLHeadingElement;
+    ele.style.transition = 'none';
+    ele.style.transform = `rotateY(${0}deg) rotateX(${0}deg)`;
+  }
+
   return (
     <header className={styles.header}>
       <section className={styles.headerNav}>
         <div className={styles.navTop}>
-          <h1>
-            <Link href='/'>Simon Fisher</Link>
-          </h1>
+          <Link href='/'>
+            <h1 onMouseMove={rotate3dEffect} onMouseLeave={setBack}>
+              Simon Fisher
+            </h1>
+          </Link>
           <div className={styles.navLinks}>
             <ul>
               {
---
+++ src/utils/throttle.ts
@@ -0,0 +1,18 @@
+export default function throttle(fn: Function, interval: number, immediate = true) {
+  let lastInvoked = 0;
+  const _throttle = function(...args: any[]) {
+    const currentTime = Date.now();
+
+    if(!immediate && lastInvoked === 0) {
+      lastInvoked = currentTime;
+    }
+
+    const waitTime = interval - (currentTime - lastInvoked);
+    if(waitTime <= 0) {
+      //@ts-ignore
+      fn.apply(this, args);
+      lastInvoked = currentTime;
+    }
+  }
+  return _throttle;
+}
