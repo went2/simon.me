d9e6b5ecb6ffd6758160f4b0ae5deb6397cf7e68
simon <simonfisher227@gmail.com>
2024-06-19 11:53:30 +0800

    feat: add app directory and root layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 // types
 import type { ReactElement, ReactNode } from 'react';
 import type { AppProps } from 'next/app';
@@ -11,17 +12,12 @@ import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Script from 'next/script';
 
-export type NextPageWithLayout<P={}, IP=P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
-
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
+export default function RootLayout({
+  children,
+}: { children: React.ReactNode }) {
   const [status, setStatus] = useState('Open');
   const siteTitle = `WestDoor ${status}`;
+
   useEffect(() => {
     // browser api need to be execucted in useEffect
     document.addEventListener('visibilitychange', () => {
@@ -33,18 +29,15 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
     });
   }, []);
 
-  const getLayout = Component.getLayout ?? ((page) => page);
-
   return (
-    <>
+    <html lang='en'>
       <Head>
         <link rel="icon" type='image/x-icon' href='/favicon.ico' />
-        <title>{siteTitle}</title>
+        <title>{ siteTitle }</title>
       </Head>
       <Script src="https://kit.fontawesome.com/db1b573488.js" crossOrigin="anonymous" />
-      {
-        getLayout(<Component {...pageProps} />)
-      }
-    </>
-  );
-};
+
+      <body> { children } </body>
+    </html>
+  )
+}
