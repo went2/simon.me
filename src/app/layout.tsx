62bb8aedff5fb9db2925252f5ef2e37486461594
simon <simonfisher227@gmail.com>
2024-06-19 14:15:36 +0800

    refactor: remove dynamic html title feature

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,40 +1,22 @@
-
 // types
-import type { ReactElement, ReactNode } from 'react';
-import type { AppProps } from 'next/app';
-import type { NextPage } from 'next';
+import type { Metadata } from 'next'
 
 // css
 import '../styles/global.scss';
 
 // components
-import { useState, useEffect } from 'react';
-import Head from 'next/head';
 import Script from 'next/script';
+
+export const metadata: Metadata = {
+  title: "Simon Fisher",
+}
 
 export default function RootLayout({
   children,
 }: { children: React.ReactNode }) {
-  const [status, setStatus] = useState('Open');
-  const siteTitle = `WestDoor ${status}`;
-
-  useEffect(() => {
-    // browser api need to be execucted in useEffect
-    document.addEventListener('visibilitychange', () => {
-      if (document.visibilityState === 'hidden') {
-        setStatus('Hidden');
-      } else {
-        setStatus('Open');
-      }
-    });
-  }, []);
 
   return (
     <html lang='en'>
-      <Head>
-        <link rel="icon" type='image/x-icon' href='/favicon.ico' />
-        <title>{ siteTitle }</title>
-      </Head>
       <Script src="https://kit.fontawesome.com/db1b573488.js" crossOrigin="anonymous" />
 
       <body> { children } </body>
